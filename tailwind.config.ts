13c716db2702c33fb8a9adaba0b23127a6ae667b
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-02-21 23:51:32 +0500

    настроил tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202A33",
+        blue: "#303F4D",
+        darkBlue: "#202A33",
+        whiteFont: "#F2F9FF",
+        blackFont: "#0C0C0D",
+        redFont: "#BC213A",
+        purpleFont: "#61378A",
+        bgWhite: "#F2F9FF",
+      },
+      fontSize: {
+        extraSm: "13px",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
