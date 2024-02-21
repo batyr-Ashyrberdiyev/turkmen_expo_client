55625aa750e401e4aaa9b0838631a331c10eb318
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-02-21 23:53:51 +0500

    добавил базу для клиента

---
+++ lib/data.ts
@@ -0,0 +1,16 @@
+export const headerMenu: {
+  title: string;
+  link: string;
+}[] = [
+  { title: "О компании", link: "/about" },
+  { title: "Новости", link: "/news" },
+  { title: "FAQ", link: "/faq" },
+  { title: "Контакты", link: "/contacts" },
+];
+
+export const headerMenu2: { title: string; link: string }[] = [
+  { title: "Календарь мероприятий", link: "/about" },
+  { title: "Участникам", link: "/news" },
+  { title: "Посетителям", link: "/faq" },
+  { title: "Услуги", link: "/contacts" },
+];
