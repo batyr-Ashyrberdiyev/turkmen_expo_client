4edae8e8e0f5b9bed80f7af62b20d6dfa975d589
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-02-22 23:40:33 +0500

    сверстал home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,35 @@
 import Image from "next/image";
 
+import heroImg from "@/public/assets/images/hero.png";
+import videoImg from "@/public/assets/images/video.png";
+
+import { Events } from "@/components/sections/Events";
+import News from "@/components/sections/News";
+import { Services } from "@/components/sections/Services";
+import { Partners } from "@/components/sections/Partners";
+import Swiper from "swiper";
+
 const Home = () => {
-  return <section className="container"></section>;
+  return (
+    <div className="bg-blueBg">
+      <Image src={heroImg} alt="hero" className="w-full h-full mb-[160px]" />
+      <section className="mb-[158px] w-full">
+        <Events />
+      </section>
+      <section className="mb-[221px]">
+        <News />
+      </section>
+      <section className="mb-[160px]">
+        <Image src={videoImg} alt="video" className="w-full mx-auto" />
+      </section>
+      <section className="mb-[160px]">
+        <Services />
+      </section>
+      <section className="pb-[160px]">
+        <Partners />
+      </section>
+    </div>
+  );
 };
 
 export default Home;
