6431810a52773162c67a530462092b9661e1fccd
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-01 15:23:55 +0500

    build tested

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,24 +1,32 @@
-"use client";
+'use client';
 
-import React from "react";
-import Image from "next/image";
+import React, { Suspense } from 'react';
+import Image from 'next/image';
 
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
-import search from "@/public/assets/icons/search.svg";
+import search from '@/public/assets/icons/search.svg';
 
-import { useAppSelector, useAppDispatch } from "@/redux/hooks";
-import { selectHeader, setShowInput } from "@/redux/slices/headerSlice";
+import { useAppSelector, useAppDispatch } from '@/redux/hooks';
+import { selectHeader, setShowInput } from '@/redux/slices/headerSlice';
 
-export const Input = () => {
+const Search = () => {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const inputRef = React.useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
-  const { showInput } = useAppSelector(selectHeader);
-  const searchParams = useSearchParams();
-  const { replace } = useRouter();
-  const pathname = usePathname();
 
-  const params = new URLSearchParams(searchParams);
+  const updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      params.set('query', e.target.value);
+    } else {
+      params.delete('query');
+    }
+
+    replace(pathname + '?' + params.toString());
+  };
 
   React.useEffect(() => {
     const handleClick = (e: MouseEvent) => {
@@ -26,20 +34,26 @@ export const Input = () => {
         dispatch(setShowInput(false));
       }
     };
-    document.body.addEventListener("click", handleClick);
+    document.body.addEventListener('click', handleClick);
 
-    return () => document.body.removeEventListener("click", handleClick);
+    return () => document.body.removeEventListener('click', handleClick);
   }, []);
 
-  const updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value) {
-      params.set("query", e.target.value);
-    } else {
-      params.delete("query");
-    }
+  return (
+    <input
+      onChange={updateValue}
+      defaultValue={searchParams.get('query')?.toString()}
+      ref={inputRef}
+      type="text"
+      placeholder="Поиск..."
+      className="rounded-[2px] py-[5px] px-[10px] focus:outline-none text-black placeholder:text-black"
+    />
+  );
+};
 
-    replace(pathname + "?" + params.toString());
-  };
+export const Input = () => {
+  const { showInput } = useAppSelector(selectHeader);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="cursor-pointer">
@@ -58,14 +72,9 @@ export const Input = () => {
         )}
       </div>
       {showInput && (
-        <input
-          onChange={updateValue}
-          defaultValue={searchParams.get("query")?.toString()}
-          ref={inputRef}
-          type="text"
-          placeholder="Поиск..."
-          className="rounded-[2px] py-[5px] px-[10px] focus:outline-none text-black placeholder:text-black"
-        />
+        <Suspense>
+          <Search />
+        </Suspense>
       )}
     </div>
   );
