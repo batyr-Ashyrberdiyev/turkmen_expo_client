ab5184ae07ccdc6f817cee5ded361ed829e24488
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-01 16:33:51 +0500

    changes to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = { images: { unoptimized: true }, distDir: 'build', output: 'export' };
 
 export default nextConfig;
