5ac9c8d5567b035b96cfe2d76b6f8e1399ba41b9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-11 17:28:12 +0500

    css changes

--- app/(root)/calendar/event/page.tsx
+++ app/(root)/calendar/event/page.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Image from "next/image";
-import { v4 as uuidv4 } from "uuid";
-import { Roboto_Slab } from "next/font/google";
+import React from 'react';
+import Image from 'next/image';
+import { Roboto_Slab } from 'next/font/google';
 
-import glassImg from "@/public/assets/images/events/CardFullImg.png";
-import glassIcon from "@/public/assets/images/events/glassIcon.png";
-import calendar from "@/public/assets/icons/calendar.svg";
+import glassImg from '@/public/assets/images/events/CardFullImg.png';
+import glassIcon from '@/public/assets/images/events/glassIcon.png';
+import calendar from '@/public/assets/icons/calendar.svg';
 
-import { BorderBtn } from "@/components/ui/Buttons";
-import { fullEventData } from "@/lib/database/eventsData";
+import { BorderBtn } from '@/components/ui/Buttons';
+import { fullEventData } from '@/lib/database/eventsData';
+import { v4 } from 'uuid';
 
 const roboto_slab = Roboto_Slab({
-  weight: ["600"],
-  subsets: ["latin", "cyrillic"],
+  weight: ['600'],
+  subsets: ['latin', 'cyrillic'],
 });
 
 const Event = () => {
@@ -35,21 +35,20 @@ const Event = () => {
           <div className=" w-full h-full z-20">
             <div className="container h-full">
               <div className="h-full flex flex-col justify-between">
-                <div className="flex items-center h-full">
-                  <div className="flex w-full h-full justify-between">
-                    <div className="w-[220px] h-[180px] bg-gray4 mr-[15px] py-[10px] px-[33px]">
-                      <Image src={glassIcon} alt="Иконка" className="mt-3" />
-                    </div>
-                    <div className="flex gap-[45px] w-full mt-[40px]">
-                      <div className="flex flex-col gap-[9px] max-w-[570px] ">
+                <div className="flex items-center">
+                  <div className="flex w-full gap-[45px]">
+                    <div className="flex gap-[15px]">
+                      <div className="w-[220px] h-[180px] bg-gray4 py-[10px] px-[33px] flex justify-center items-center">
+                        <Image src={glassIcon} width={154} height={160} alt="Иконка" className="" />
+                      </div>
+                      <div className="flex flex-col gap-[9px] max-w-[580px] w-full mt-[40px]">
                         <h3
-                          className={`${roboto_slab.className} font-semibold text-[34px] leading-[100%]`}
-                        >
+                          className={`${roboto_slab.className} font-semibold text-[34px] leading-[100%]`}>
                           Мир Стекла-2024
                         </h3>
                         <h4 className="text-[16px] leading-[150%]">
-                          25-я международная выставка стеклопродукции технлогий
-                          и оборудования для изготовления стекла
+                          25-я международная выставка стеклопродукции технлогий и оборудования для
+                          изготовления стекла
                         </h4>
 
                         <div className="text-[10px] flex flex-col gap-[5px]">
@@ -57,25 +56,23 @@ const Event = () => {
                           <p>АО «ЭКСПОЦЕНТР»</p>
                         </div>
                       </div>
-                      <div className="flex flex-col w-full max-w-[238px]">
-                        <h3 className="text-[21px] font-semibold mb-5 leading-[100%]">
-                          27 февраля - 1 марта 2024
-                        </h3>
-                        <div className="flex items-center gap-[10px]">
-                          <Image src={calendar} alt="calendar" />
-                          <p className="text-gray4">
-                            Добавить в календарь (ics файл)
-                          </p>
-                        </div>
+                    </div>
+                    <div className="flex flex-col w-full max-w-[238px] mt-[40px]">
+                      <h3 className="text-[21px] font-semibold mb-5 leading-[100%]">
+                        27 февраля - 1 марта 2024
+                      </h3>
+                      <div className="flex items-center gap-[10px]">
+                        <Image src={calendar} alt="calendar" />
+                        <p className="text-gray4">Добавить в календарь (ics файл)</p>
                       </div>
                     </div>
                   </div>
                 </div>
 
                 <div className="flex gap-5 mb-[20px]">
-                  <BorderBtn text={"Сайт выставки"} />
-                  <BorderBtn text={"Забронировать стенд"} />
-                  <BorderBtn text={"Получить электронный билет"} />
+                  <BorderBtn text={'Сайт выставки'} />
+                  <BorderBtn text={'Забронировать стенд'} />
+                  <BorderBtn text={'Получить электронный билет'} />
                 </div>
               </div>
             </div>
@@ -87,19 +84,12 @@ const Event = () => {
             {fullEventData
               .filter((item) => item.arrangement)
               .map((item) => (
-                <div key={uuidv4()}>
-                  <p className="mb-[40px] leading-[100%] text-[21px] font-semibold">
-                    {item.title}
-                  </p>
+                <div key={v4()}>
+                  <p className="mb-[40px] leading-[100%] text-[21px] font-semibold">{item.title}</p>
                   <div className="flex gap-[20px]">
                     {item.info?.map((text) => (
-                      <div
-                        key={uuidv4()}
-                        className="w-full max-w-[290px] flex flex-col gap-[20px]"
-                      >
-                        <h3 className="leading-[120%] text-[18px] font-semibold">
-                          {text.title}
-                        </h3>
+                      <div key={v4()} className="w-full max-w-[290px] flex flex-col gap-[20px]">
+                        <h3 className="leading-[120%] text-[18px] font-semibold">{text.title}</h3>
                         <div className="leading-[130%]">
                           <p className="text-gray4">{text.date}</p>
                           <p className="text-gray">{text.time}</p>
@@ -119,17 +109,12 @@ const Event = () => {
                 .filter((item) => item.theme)
                 .map((obj) => (
                   <>
-                    <h3
-                      key={uuidv4()}
-                      className="text-[21px] leading-[100%] font-semibold"
-                    >
+                    <h3 key={v4()} className="text-[21px] leading-[100%] font-semibold">
                       {obj.title}
                     </h3>
                     {obj.info?.map((text) => (
-                      <div key={uuidv4()} className="flex flex-col gap-[20px]">
-                        <h4 className="text-[18px] font-semibold leading-[120%]">
-                          {text.title}
-                        </h4>
+                      <div key={v4()} className="flex flex-col gap-[20px]">
+                        <h4 className="text-[18px] font-semibold leading-[120%]">{text.title}</h4>
 
                         <div className="pl-[9px] leading-[130%] list-disc">
                           <div className="text-gray4 relative">
@@ -161,7 +146,7 @@ const Event = () => {
               {fullEventData
                 .filter((item) => item.address)
                 .map((obj) => (
-                  <div className="flex flex-col gap-[10px]">
+                  <div className="flex flex-col gap-[10px]" key={v4()}>
                     <h3 className="text-21 mb-[10px]">{obj.title}</h3>
                     <h4 className="text-gray leading-[130%]">{obj.place}</h4>
                     <div className="leading-[130%]">
