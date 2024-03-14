7d0f32ef1795bde7c49aa2db8aaa663fda9df565
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-03-14 18:14:16 +0500

    Добавил пропсы

--- components/bid/BidRadio.tsx
+++ components/bid/BidRadio.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
 
-export const BidRadio = ({ text }: { text: string }) => {
+import React from "react";
+import clsx from "clsx";
+
+import { useFormContext } from "react-hook-form";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { selectBid } from "@/redux/slices/bidSlice";
+
+interface Props {
+  text: string;
+  id: string;
+  onRadio: (name: string) => void;
+}
+
+export const BidRadio = ({ text, id, onRadio }: Props) => {
+  const { radioStatus } = useAppSelector(selectBid);
+  const { setValue } = useFormContext();
+
   return (
-    <div className="flex items-center gap-[10px] cursor-pointer">
+    <div
+      onClick={() => {
+        onRadio(id), setValue("radio", id);
+      }}
+      className="flex items-center gap-[10px] cursor-pointer"
+    >
       <div className="p-[3px] rounded-full w-[16px] h-[16px] border-[1px] border-navyBlue cursor-pointer">
         <div
-          className={`transition-opacity opacity-0 ${"opacity-100"} bg-green h-full w-full rounded-full`}
+          className={clsx(
+            `transition-all opacity-0 bg-green h-full w-full rounded-full`,
+            { "opacity-100": radioStatus === id }
+          )}
         ></div>
       </div>
-      <div>{text}</div>
+      <div className="leading-[125%] text-extraSm">{text}</div>
     </div>
   );
 };
