99b2efff3e499080c8c934498c38941cc10fef53
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-16 15:01:45 +0500

    none

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import StoreProvider from "./StoreProvider";
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import StoreProvider from './StoreProvider';
 
-import "./globals.css";
+import './globals.css';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "TurkmenExpo",
-  description: "",
-  icons: "",
+  title: 'TurkmenExpo',
+  description: '',
+  icons: '',
 };
 
 export default function RootLayout({
