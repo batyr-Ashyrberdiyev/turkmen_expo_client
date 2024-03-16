7e554b82c6ae9e3d7f8d4dd6d1c44cdde146c269
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-16 13:13:58 +0500

    изиенил тень

--- components/ui/Buttons.tsx
+++ components/ui/Buttons.tsx
@@ -1,31 +1,27 @@
-"use client";
+'use client';
 
-import React from "react";
-import clsx from "clsx";
+import React from 'react';
+import clsx from 'clsx';
 
-export const GreenBtn = ({
-  text,
-  mt,
-  onEventBtn,
-  px,
-}: {
+interface GreenBtnProps {
   text: string;
   mt?: string;
   onEventBtn?: () => void;
   border?: boolean;
   px?: boolean;
-}) => {
+}
+
+export const GreenBtn = ({ text, mt, onEventBtn, px }: GreenBtnProps) => {
   return (
     <button
       onClick={onEventBtn}
       className={clsx(
-        `${mt} drop-shadow-md py-[17px] text-[14px] rounded-sm bg-green text-bgWhite hover:bg-lightGreen font-medium transition-all`,
+        `${mt}  py-[17px] text-[14px] greenBtnShadow rounded-sm bg-green text-bgWhite hover:bg-lightGreen font-medium transition-all`,
         {
-          "px-[43px]": !px,
-          "px-[70px]": px,
-        }
-      )}
-    >
+          'px-[43px]': !px,
+          'px-[70px]': px,
+        },
+      )}>
       {text}
     </button>
   );
@@ -41,11 +37,8 @@ interface BorderProps {
 export const BorderBtn = ({ onEventBtn, text, mt, px }: BorderProps) => {
   return (
     <button
-      className={`mt-[${mt} border-btn py-[17px] ${
-        px ? "px-[43px]" : "px-[17px]"
-      }`}
-      onClick={onEventBtn}
-    >
+      className={`mt-[${mt} border-btn py-[17px] ${px ? 'px-[43px]' : 'px-[17px]'}`}
+      onClick={onEventBtn}>
       {text}
     </button>
   );
