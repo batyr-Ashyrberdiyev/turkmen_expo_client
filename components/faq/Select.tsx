2b0f7b730f3c22dfb50692c82300bf2e828a2856
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-03-18 17:01:31 +0500

    удалил лишний код

--- components/faq/Select.tsx
+++ components/faq/Select.tsx
@@ -1,60 +1,67 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import clsx from 'clsx';
-import { v4 } from 'uuid';
+import React from "react";
+import Image from "next/image";
+import clsx from "clsx";
+import { v4 } from "uuid";
 
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
-import add from '@/public/assets/icons/faq/add.svg';
-import minus from '@/public/assets/icons/faq/minus.svg';
-import openIcon from '@/public/assets/icons/contact-arrow.svg';
+import add from "@/public/assets/icons/faq/add.svg";
+import minus from "@/public/assets/icons/faq/minus.svg";
+import openIcon from "@/public/assets/icons/contact-arrow.svg";
 
-import { faqData } from '@/lib/database/faqData';
-import { selectFaq, setFaqInfo } from '@/redux/slices/faqSlice';
+import { FAQItem, faqData } from "@/lib/database/faqData";
+import { selectFaq, setFaqInfo, setFaqTitle } from "@/redux/slices/faqSlice";
+import PlusDrop from "./PlusDrop";
 
 interface Props {
   visitors?: boolean;
   members?: boolean;
   all?: boolean;
-  onTitle: (name: string) => void;
 }
 
-export const Select = ({ onTitle, visitors = false, members = false, all = false }: Props) => {
+export const Select = ({
+  visitors = false,
+  members = false,
+  all = false,
+}: Props) => {
   const dispatch = useAppDispatch();
   const { faqTitle, faqInfo } = useAppSelector(selectFaq);
 
-  const [value, setValue] = React.useState([]);
-
-  const setText = (name: string) => {
-    if (name === faqInfo) {
-      dispatch(setFaqInfo(''));
-    } else {
-      dispatch(setFaqInfo(name));
-    }
+  const onTitle = (name: string) => {
+    if (faqTitle === name) dispatch(setFaqTitle(""));
+    else dispatch(setFaqTitle(name));
   };
 
   return (
     <div className="w-full">
       {faqData
-        .filter((item) => (visitors && item.visitors) || (members && item.members) || (all && item))
+        .filter(
+          (item) =>
+            (visitors && item.visitors) ||
+            (members && item.members) ||
+            (all && item)
+        )
         .map((item) => (
           <div key={v4()} className="w-full">
             <div
               onClick={() => onTitle(item.title)}
               className={clsx(
-                'w-full flex items-center justify-between border-y-[1px] border-y-navyBlue cursor-pointer',
+                "w-full flex items-center justify-between border-y-[1px] border-y-navyBlue cursor-pointer",
                 {
-                  'border-t-navyBlue5': faqTitle === item.title,
-                },
-              )}>
-              <h2 className="text-[21px] font-semibold py-[20px]">{item.title}</h2>
+                  "border-t-navyBlue5": faqTitle === item.title,
+                }
+              )}
+            >
+              <h2 className="text-[21px] font-semibold py-[20px]">
+                {item.title}
+              </h2>
               <Image
                 src={openIcon}
                 alt="arrow"
-                className={clsx('rotate-[180deg] transition-all', {
-                  'rotate-[360deg]': faqTitle === item.title,
+                className={clsx("rotate-[180deg] transition-all", {
+                  "rotate-[360deg]": faqTitle === item.title,
                 })}
               />
             </div>
@@ -63,10 +70,12 @@ export const Select = ({ onTitle, visitors = false, members = false, all = false
                 item.faqAll.map((obj) => (
                   <div
                     key={v4()}
-                    className="text-gray4 px-[40px] first-of-type:pt-[30px] flex flex-col items-start gap-y-[16px] leading-[1.5]">
+                    className="text-gray4 px-[40px] first-of-type:pt-[30px] flex flex-col items-start gap-y-[16px] leading-[1.5]"
+                  >
                     <div
-                      onClick={() => setFaqInfo('')}
-                      className="flex items-center gap-x-[10px] cursor-pointer">
+                      onClick={() => setFaqInfo("")}
+                      className="flex items-center gap-x-[10px] cursor-pointer"
+                    >
                       {!item.wifi && (
                         <Image
                           src={faqInfo === item.title ? minus : add}
@@ -77,27 +86,7 @@ export const Select = ({ onTitle, visitors = false, members = false, all = false
                       )}
                       <h4 className="text-[16px] text-bgWhite">{obj.title}</h4>
                     </div>
-                    {faqInfo === item.title && (
-                      <>
-                        <h4
-                          // onClick={() => setText()}
-                          className={clsx({
-                            'text-[16px] text-bgWhite ': item.wifi,
-                          })}>
-                          {obj.text}
-                        </h4>
-                        <div>
-                          {obj.text2 && <h4>{obj.text2}</h4>}
-                          {obj.text3 && <h4>{obj.text3}</h4>}
-                          {obj.text4 && <h4>{obj.text4}</h4>}
-                          {obj.list && <p className="faq-list">{obj.list}</p>}
-                          {obj.list2 && <p className="faq-list">{obj.list2}</p>}
-                          {obj.list3 && <p className="faq-list">{obj.list3}</p>}
-                          {obj.list4 && <p className="faq-list">{obj.list4}</p>}
-                          {obj.text5 && <h4>{obj.text5}</h4>}
-                        </div>
-                      </>
-                    )}
+                    <PlusDrop {...obj} />
                   </div>
                 ))}
             </div>
