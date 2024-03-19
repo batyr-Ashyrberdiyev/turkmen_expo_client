de7044abec927aeda04ab2a2d6047245fc60a1eb
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-19 18:32:04 +0500

    новая страница news

--- app/(root)/(mainMenu)/news/page.tsx
+++ app/(root)/(mainMenu)/news/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+
+import { PageLayout } from '@/components/page/PageLayout';
+import { NewsSec } from '@/components/news/NewsSec';
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div>
+      <PageLayout pathname={'Главная / Пресс-центр'} title={'Новости'}>
+        <NewsSec />
+      </PageLayout>
+    </div>
+  );
 };
 
 export default News;
