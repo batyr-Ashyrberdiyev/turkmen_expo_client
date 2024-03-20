03f9583b2d5b989633af076b42c832364077dca2
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-03-20 18:22:26 +0500

    adaptiw

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -1,14 +1,12 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { v4 } from 'uuid';
+import React, { useState } from "react";
+import Image from "next/image";
+import { v4 } from "uuid";
 
-import { contactCardData, roadCardData } from '@/lib/database/homeInfoData';
+import { contactCardData, roadCardData } from "@/lib/database/homeInfoData";
 
 export const Services = () => {
-  const [hover, setHover] = React.useState(false);
-
   return (
     <div className={`container flex gap-x-[40px] items-center`}>
       <div className="flex flex-col items-start gap-x-[20px] w-1/2">
@@ -19,7 +17,8 @@ export const Services = () => {
           {roadCardData.map((item) => (
             <div
               className="bg-white transition-all hover:hover-shadow cursor-pointer service-shadow px-[40px] py-[20px] text-black greenBtnShadow rounded-[2px] custom-shadow"
-              key={v4()}>
+              key={v4()}
+            >
               <div className="flex items-center gap-x-[40px]">
                 <Image className="img-auto" src={item.icon} alt="icon" />
                 <p className="text-[16px]">{item.text}</p>
@@ -36,7 +35,8 @@ export const Services = () => {
           {contactCardData.map((item) => (
             <div
               className="bg-white transition-all hover:hover-shadow cursor-pointer service-shadow px-[40px] py-[20px] text-black greenBtnShadow rounded-[2px] "
-              key={v4()}>
+              key={v4()}
+            >
               <div className="flex items-center gap-x-[40px]">
                 <Image src={item.icon} alt="icon" />
                 <p className="text-[16px]">{item.text}</p>
