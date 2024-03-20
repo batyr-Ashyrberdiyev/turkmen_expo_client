e29f3eeff1baab518aa311414fa3bd5bc7ff9654
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-03-20 15:47:26 +0500

    no change

--- components/home/Events.tsx
+++ components/home/Events.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { v4 } from 'uuid';
-import { EventCard } from '../cards/EventCard';
-import { GreenBtn } from '../ui/Buttons';
-import { eventCardData } from '@/lib/database/eventsData';
+import React from "react";
+import { v4 } from "uuid";
+import { EventCard } from "../cards/EventCard";
+import { GreenBtn } from "../ui/Buttons";
+import { eventCardData } from "@/lib/database/eventsData";
 
 export const Events = () => {
   const [openCards, setOpenCards] = React.useState<boolean>(true);
@@ -14,19 +14,45 @@ export const Events = () => {
   };
 
   return (
-    <div className="container">
-      <h2 className="text-extra mb-[40px] font-semibold leading-[100%]">
-        Ближайшие выставки и мероприятия
-      </h2>
-      <div className="mb-[158px] flex flex-col items-center gap-y-[10px]">
-        {openCards
-          ? eventCardData
-              .filter((item) => item.id === 1 || item.id === 2)
-              .map((item) => <EventCard key={v4()} {...item} />)
-          : eventCardData.map((item) => <EventCard key={v4()} {...item} />)}
+    <>
+      <div className="container">
+        <h2 className="text-extra mb-[40px] font-semibold leading-[100%]">
+          Ближайшие выставки и мероприятия
+        </h2>
+        <div className="mb-[158px] flex flex-col items-center gap-y-[10px]">
+          {openCards
+            ? eventCardData
+                .filter((item) => item.id === 1 || item.id === 2)
+                .map((item) => <EventCard key={v4()} {...item} />)
+            : eventCardData.map((item) => <EventCard key={v4()} {...item} />)}
 
-        <GreenBtn onEventBtn={onEventBtn} text={'Показать ещё'} mt="mt-[25px]" />
+          <GreenBtn
+            onEventBtn={onEventBtn}
+            text={"Показать ещё"}
+            mt="mt-[25px]"
+          />
+        </div>
       </div>
-    </div>
+
+      {/* //Mobile */}
+      <div className="container">
+        <h2 className="text-extra mb-[40px] font-semibold leading-[100%]">
+          Ближайшие выставки и мероприятия
+        </h2>
+        <div className="mb-[158px] flex flex-col items-center gap-y-[10px]">
+          {openCards
+            ? eventCardData
+                .filter((item) => item.id === 1 || item.id === 2)
+                .map((item) => <EventCard key={v4()} {...item} />)
+            : eventCardData.map((item) => <EventCard key={v4()} {...item} />)}
+
+          <GreenBtn
+            onEventBtn={onEventBtn}
+            text={"Показать ещё"}
+            mt="mt-[25px]"
+          />
+        </div>
+      </div>
+    </>
   );
 };
--- components/news/NewsSec.tsx
+++ components/news/NewsSec.tsx
@@ -1,29 +1,32 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import { v4 } from 'uuid';
-import clsx from 'clsx';
+import React from "react";
+import Image from "next/image";
+import { v4 } from "uuid";
+import clsx from "clsx";
 
-import { newsPageData } from '@/lib/database/newsData';
-import Card from './Card';
-import { BorderBtn } from '../ui/Buttons';
-import Pagination from '../ui/Pagination';
+import { newsPageData } from "@/lib/database/newsData";
+import Card from "./Card";
+import { BorderBtn } from "../ui/Buttons";
+import Pagination from "../ui/Pagination";
 
-import message from '@/public/assets/icons/news/message.svg';
-import search from '@/public/assets/icons/news/search.svg';
-import col from '@/public/assets/icons/news/col.svg';
-import gridIco from '@/public/assets/icons/news/grid.svg';
+import message from "@/public/assets/icons/news/message.svg";
+import search from "@/public/assets/icons/news/search.svg";
+import col from "@/public/assets/icons/news/col.svg";
+import gridIco from "@/public/assets/icons/news/grid.svg";
 
 export const NewsSec = () => {
-  const menu = ['Новости', 'СМИ о нас'];
+  const menu = ["Новости", "СМИ о нас"];
 
-  const [grid, setGrid] = React.useState(false);
+  const [grid, setGrid] = React.useState(true);
 
   return (
     <div>
       <div className="flex flex-col">
-        <input className="bid-input w-full max-w-[461px] mb-[60px]" type="text" />
+        <input
+          className="bid-input w-full max-w-[461px] mb-[60px]"
+          type="text"
+        />
 
         <div className="flex flex-col">
           <div className="flex justify-between items-center mb-[24px] pb-[5px]">
@@ -36,26 +39,30 @@ export const NewsSec = () => {
               onClick={() => setGrid(!grid)}
               className="cursor-pointer"
               src={!grid ? gridIco : col}
-              alt="сетка"></Image>
+              alt="сетка"
+            ></Image>
           </div>
 
           <div className="flex w-full max-w-[184px] items-center cursor-pointer justify-center mx-auto mb-[24px] gap-[10px]">
             <Image src={message} alt="Письмо"></Image>
-            <p className="leading-[125%] text-extraSm">подписаться на новости</p>
+            <p className="leading-[125%] text-extraSm">
+              подписаться на новости
+            </p>
           </div>
 
           <div
-            className={clsx('mb-[108px]', {
-              'flex flex-col gap-6': !grid,
-              'grid grid-cols-2 gap-y-[85px] gap-x-6': grid,
-            })}>
+            className={clsx("mb-[108px]", {
+              "flex flex-col gap-6": !grid,
+              "grid grid-cols-2 gap-y-[85px] gap-x-6": grid,
+            })}
+          >
             {newsPageData.map((item) => (
               <Card grid={grid} key={v4()} {...item} />
             ))}
           </div>
 
           <div className="flex flex-col gap-6 w-full max-w-[180px] mx-auto justify-center">
-            <BorderBtn text={'Показать ещё'} />
+            <BorderBtn text={"Показать ещё"} />
             <Pagination />
           </div>
         </div>
