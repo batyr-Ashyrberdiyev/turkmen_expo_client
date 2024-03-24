b87f4608a66046a12507cb33cf409382881d68cc
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-03-24 19:28:49 +0500

    добавил стрелочки для ссылок

--- components/ui/BurgerMenu.tsx
+++ components/ui/BurgerMenu.tsx
@@ -8,6 +8,7 @@ import { v4 } from "uuid";
 import ru from "@/public/assets/icons/header/ru.svg";
 import en from "@/public/assets/icons/header/en.svg";
 import tm from "@/public/assets/icons/header/tm.svg";
+import arrow from "@/public/assets/icons/header/burger-arrow.svg";
 import { headerMenu, headerMenu2 } from "@/lib/database/pathnames";
 
 export const flags = [
@@ -21,8 +22,13 @@ export const BurgerMenu = () => {
     <div className="bg-green absolute w-full z-[150] top-[74px] left-0 h-svh px-4 py-10 flex flex-col gap-10">
       <div className="flex flex-col gap-5">
         {headerMenu2.map((item) => (
-          <Link href={item.link} key={v4()} className="cursor-pointer">
-            {item.title}
+          <Link
+            href={item.link}
+            key={v4()}
+            className="cursor-pointer flex items-center justify-between"
+          >
+            <p className="text-[18px] leading-[135%]">{item.title}</p>
+            {!item.one && <Image src={arrow} alt="стрелка" />}
           </Link>
         ))}
       </div>
@@ -31,8 +37,12 @@ export const BurgerMenu = () => {
 
       <div className="flex flex-col gap-5">
         {headerMenu.map((item) => (
-          <Link className="cursor-pointer" href={item.link}>
-            {item.title}
+          <Link
+            className="cursor-pointer flex items-center justify-between"
+            href={item.link}
+          >
+            <p>{item.title}</p>
+            <Image src={arrow} alt="стрелка" width={20} height={20} />
           </Link>
         ))}
       </div>
